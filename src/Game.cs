d1b8809da830cd06cf1e27f4e5cc3885d3f17e12
Robin <1121080+rYuuk@users.noreply.github.com>
2022-10-27 05:22:35 +0530

    Add fps camera

--- src/Game.cs
+++ src/Game.cs
@@ -66,27 +66,18 @@ namespace OpenGLEngine
         private Shader shader;
         private Texture texture;
         private Texture texture2;
-
-        private Matrix4 view;
-        private Matrix4 projection;
-
         private double time;
+
+        // Instance of the camera class to manage the view and projection matrix code.
+        private Camera camera;
+        // A boolean set to true to detect whether or not the mouse has been moved for the first time.
+        private bool firstMove;
+        // The last position of the mouse so we can calculate the mouse offset easily.
+        private Vector2 lastPos;
 
         public Game(int width, int height, string title) :
             base(GameWindowSettings.Default, new NativeWindowSettings() { Size = (width, height), Title = title })
         {
-        }
-
-        protected override void OnUpdateFrame(FrameEventArgs args)
-        {
-            base.OnUpdateFrame(args);
-
-            var input = KeyboardState;
-
-            if (input.IsKeyDown(Keys.Escape))
-            {
-                Close();
-            }
         }
 
         protected override void OnLoad()
@@ -94,7 +85,7 @@ namespace OpenGLEngine
             base.OnLoad();
 
             GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-            
+
             // Enable depth testing so z-buffer can be checked for fragments and only those which are in front be drawn.
             GL.Enable(EnableCap.DepthTest);
 
@@ -133,11 +124,11 @@ namespace OpenGLEngine
             shader.SetInt("texture0", 0);
             shader.SetInt("texture1", 1);
 
-            // Set the view
-            view = Matrix4.CreateTranslation(0, 0, -3f);
+            // Initialize the camera so that it is 3 units back from where the rectangle is.
+            camera = new Camera(Vector3.UnitZ * 3, Size.X / (float) Size.Y, 1.5f, 0.2f);
 
-            // Perspective projection
-            projection = Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(45), (float) Size.X / Size.Y, 0.1f, 100f);
+            // To make the mouse cursor invisible and captured so to have proper FPS-camera movement.
+            CursorState = CursorState.Grabbed;
         }
 
         protected override void OnUnload()
@@ -150,7 +141,7 @@ namespace OpenGLEngine
         {
             base.OnRenderFrame(args);
 
-            time += 4 * args.Time;
+            time += 8 * args.Time;
 
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
             GL.BindVertexArray(vertexArrayObject);
@@ -163,17 +154,102 @@ namespace OpenGLEngine
             var model = Matrix4.CreateRotationX((float) MathHelper.DegreesToRadians(time));
 
             shader.SetMatrix4("model", model);
-            shader.SetMatrix4("view", view);
-            shader.SetMatrix4("projection", projection);
+            shader.SetMatrix4("view", camera.GetViewMatrix());
+            shader.SetMatrix4("projection", camera.GetProjectionMatrix());
 
             GL.DrawArrays(PrimitiveType.Triangles, 0, vertices.Length);
             SwapBuffers();
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+
+            if (!IsFocused)
+            {
+                return;
+            }
+
+            var input = KeyboardState;
+
+            if (input.IsKeyDown(Keys.Escape))
+            {
+                Close();
+            }
+
+            if (input.IsKeyDown(Keys.W))
+            {
+                camera.Position += camera.Front * camera.Speed * (float) e.Time; // Forward
+            }
+
+            if (input.IsKeyDown(Keys.S))
+            {
+                camera.Position -= camera.Front * camera.Speed * (float) e.Time; // Backwards
+            }
+            if (input.IsKeyDown(Keys.A))
+            {
+                camera.Position -= camera.Right * camera.Speed * (float) e.Time; // Left
+            }
+            if (input.IsKeyDown(Keys.D))
+            {
+                camera.Position += camera.Right * camera.Speed * (float) e.Time; // Right
+            }
+            if (input.IsKeyDown(Keys.Space))
+            {
+                camera.Position += camera.Up * camera.Speed * (float) e.Time; // Up
+            }
+            if (input.IsKeyDown(Keys.LeftShift))
+            {
+                camera.Position -= camera.Up * camera.Speed * (float) e.Time; // Down
+            }
+
+            if (input.IsKeyDown(Keys.Space))
+            {
+                camera.Position += camera.Up * camera.Speed * (float) e.Time;
+            }
+
+            if (input.IsKeyDown(Keys.LeftShift))
+            {
+                camera.Position -= camera.Up * camera.Speed * (float) e.Time;
+            }
+
+            // Get the mouse state
+            var mouse = MouseState;
+
+            if (firstMove)
+            {
+                lastPos = new Vector2(mouse.X, mouse.Y);
+                firstMove = false;
+            }
+            else
+            {
+                var deltaX = mouse.X - lastPos.X;
+                var deltaY = mouse.Y - lastPos.Y;
+                lastPos = new Vector2(mouse.X, mouse.Y);
+
+                // Apply the camera pitch and yaw (we clamp the pitch in the camera class)
+                camera.Yaw += deltaX * camera.Sensitivity;
+                // Reversed since y-coordinates range from bottom to top
+                camera.Pitch -= deltaY * camera.Sensitivity;
+            }
+        }
+
+        // This manage all the zooming of the camera.
+        // This is simply done by changing the FOV of the camera.
+        protected override void OnMouseWheel(MouseWheelEventArgs e)
+        {
+            base.OnMouseWheel(e);
+            camera.Fov -= e.OffsetY;
         }
 
         protected override void OnResize(ResizeEventArgs e)
         {
             base.OnResize(e);
             GL.Viewport(0, 0, e.Width, e.Height);
+
+            // Update the aspect ratio once the window has been resized.
+            camera.AspectRatio = Size.X / (float) Size.Y;
+
         }
     }
 }
