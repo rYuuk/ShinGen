d679f7851acb01c2987402e87f356450a037c58d
Robin <1121080+rYuuk@users.noreply.github.com>
2022-12-08 19:56:21 +0100

    Add cube renderer

---
+++ src/render/CubeRenderer.cs
@@ -0,0 +1,49 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+
+namespace OpenGLEngine
+{
+    public class CubeRenderer
+    {
+        private readonly VertexArray vertexArray;
+        private readonly Shader shader;
+
+        public CubeRenderer()
+        {
+            vertexArray = new VertexArray();
+            shader = new Shader(
+                "src/shaders/cubeShader.vert",
+                "src/shaders/cubeShader.frag");
+        }
+
+        public void Load()
+        {
+            vertexArray.Load();
+
+            var vertexBuffer = new VertexBuffer<float>(sizeof(float) * VertexData.CubeWithNormalAndTexCoord.Length, VertexData.CubeWithNormalAndTexCoord);
+            vertexBuffer.Load();
+
+            var vertexLayout = new VertexBufferLayout();
+            vertexLayout.Push(0,3);
+            vertexLayout.Push(1,3);
+            vertexLayout.Push(2,2);
+
+            vertexArray.AddBufferLayout(vertexLayout);
+            vertexArray.UnLoad();
+            
+            shader.Load();
+        }
+
+        public void Draw(Matrix4 view, Matrix4 projection)
+        {
+            shader.Bind();
+            shader.SetMatrix4("model",  Matrix4.Identity);
+            shader.SetMatrix4("view", view);
+            shader.SetMatrix4("projection", projection);
+
+            vertexArray.Load();
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 36);
+            vertexArray.UnLoad();
+        }
+    }
+}
---
+++ src/shaders/cubeShader.frag
@@ -0,0 +1,8 @@
+﻿#version 330
+
+out vec4 FragColor;
+
+void main()
+{
+    FragColor = vec4(1, 0, 0, 1);
+}
