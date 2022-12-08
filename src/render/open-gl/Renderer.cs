193dc954ce46e77feed0b4b3f49aae001a08f486
Robin <1121080+rYuuk@users.noreply.github.com>
2022-12-08 19:56:43 +0100

    Enable back face culling

--- src/render/open-gl/Renderer.cs
+++ src/render/open-gl/Renderer.cs
@@ -20,11 +20,18 @@ namespace OpenGLEngine
                 // Enable depth testing so z-buffer can be checked for fragments and
                 // only those which are in front be drawn.
                 GL.Enable(EnableCap.DepthTest);
+
+                // GL.Enable(EnableCap.Blend);
+                // GL.BlendFunc(BlendingFactor.SrcAlpha, BlendingFactor.OneMinusSrcAlpha);
+                
+                GL.Enable(EnableCap.CullFace);
+                GL.CullFace(CullFaceMode.Back);
             }
         }
 
         public void Clear()
         {
+            GL.ClearColor(0.1f, 0.1f, 0.1f, 1.0f);
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
         }
 
